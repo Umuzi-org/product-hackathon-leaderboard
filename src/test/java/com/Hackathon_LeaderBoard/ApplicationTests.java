65f15cb566fd3de445a752f8db4c497fd40a0076
latifa <simbinelatifa@gmail.com>
2020-02-05 15:30:14 +0200

    initial commit

---
+++ src/test/java/com/Hackathon_LeaderBoard/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hackathon_LeaderBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
