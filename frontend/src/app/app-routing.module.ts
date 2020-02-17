03cb0e1260c9b836825a38119bca994c89f32aa6
Thotsana <thotsana.mabotsa@umuzi.org>
2020-02-17 13:42:31 +0200

    added route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationFormComponent} from './registration-form/registration-form.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register',component: RegistrationFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
