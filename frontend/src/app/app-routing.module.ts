c40ba5519ca37494540b85d37bae38389bbbe13f
Kagiso Raseroka <kagiso.raseroka@umuzi.org>
2020-02-21 11:26:32 +0200

    Updating

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"leaderboard",component:LeaderboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
