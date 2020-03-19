ddada69d5767c2bfca7c034ca0749c5c76637c4b
aldrinbgama <aldrin.gama@umuzi.org>
2020-03-19 03:52:37 +0200

    model

--- backend/src/main/java/com/Hackathon_LeaderBoard/model/Recruit.java
+++ backend/src/main/java/com/Hackathon_LeaderBoard/model/Recruit.java
@@ -1,2 +1,43 @@
 package com.Hackathon_LeaderBoard;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+public class Recruit {
+    private final UUID id;
+    @NotBlank
+    private final String name, surname;
+    private final String username, password;
+
+    public Recruit(@JsonProperty("id") UUID id,
+                   @JsonProperty("name") String name,
+                   @JsonProperty("surname") String surname,
+                   @JsonProperty("username") String username,
+                   @JsonProperty("password") String password) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+    }
+
+    public UUID getId() { return id; }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
