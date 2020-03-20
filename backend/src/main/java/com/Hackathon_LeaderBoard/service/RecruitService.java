9abc04a5e54e3d12e591e6c363d279896ba8e99a
aldrinbgama <aldrin.gama@umuzi.org>
2020-03-20 06:45:07 +0200

    isValidUser

--- backend/src/main/java/com/Hackathon_LeaderBoard/service/RecruitService.java
+++ backend/src/main/java/com/Hackathon_LeaderBoard/service/RecruitService.java
@@ -1,41 +1,7 @@
 package com.heckathon.service;
 
-import com.heckathon.dao.RecruitDao;
-import com.heckathon.model.Recruit;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+import java.sql.SQLException;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-@Service
-public class RecruitService {
-    private final RecruitDao recruitDao;
-
-    @Autowired
-    public RecruitService(@Qualifier("postgres") RecruitDao recruitDao) {
-        this.recruitDao = recruitDao;
-    }
-
-    public int addRecruit(Recruit recruit) {
-        return recruitDao.insertRecruit(recruit);
-    }
-
-    public List<Recruit> getAllRecruits () {
-        return recruitDao.selectAllRecruits();
-    }
-
-    public Optional<Recruit> getRecruitById(UUID id) {
-        return recruitDao.selectRecruitById(id);
-    }
-
-    public int deleteRecruit(UUID id) {
-        return recruitDao.deleteRecruitById(id);
-    }
-
-    public int updateRecruit(UUID id, Recruit newRecruit) {
-        return recruitDao.updateRecruitById(id, newRecruit);
-    }
+public interface UserService {
+    public boolean isValidUser(String username, String password) throws SQLException;
 }
---
+++ backend/src/main/java/com/Hackathon_LeaderBoard/service/RecruitServiceImplementation.java
@@ -0,0 +1,47 @@
+package com.heckathon.service;
+
+import com.heckathon.dao.RecruitDao;
+import com.heckathon.model.Recruit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RecruitService implements RecruitService {
+    private final RecruitDao recruitDao;
+
+    @Autowired
+    public RecruitService(@Qualifier("postgres") RecruitDao recruitDao) {
+        this.recruitDao = recruitDao;
+    }
+
+    public int addRecruit(Recruit recruit) {
+        return recruitDao.insertRecruit(recruit);
+    }
+
+    public List<Recruit> getAllRecruits () {
+        return recruitDao.selectAllRecruits();
+    }
+
+    public Optional<Recruit> getRecruitById(UUID id) {
+        return recruitDao.selectRecruitById(id);
+    }
+
+    public int deleteRecruit(UUID id) {
+        return recruitDao.deleteRecruitById(id);
+    }
+
+    public int updateRecruit(UUID id, Recruit newRecruit) {
+        return recruitDao.updateRecruitById(id, newRecruit);
+    }
+
+    @Override
+    public boolean isValidUser(String username, String password) throws SQLException
+    {
+        return userDao.isValidUser(username, password);
+    }
+}
