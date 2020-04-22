dce56e746469b7fe92f780a4931b2853b7324406
mdu <MduduziMth@gmail.com>
2020-04-22 11:55:13 +0200

    added user exists validation

---
+++ src/main/java/com/Hackathon_LeaderBoard/controller/RecruitController.java
@@ -0,0 +1,21 @@
+package com.Hackathon_LeaderBoard.controller;
+
+import com.Hackathon_LeaderBoard.model.Recruit;
+import com.Hackathon_LeaderBoard.service.RecruitUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecruitController {
+
+    @Autowired
+    private RecruitUserServiceImpl recruitUserService;
+
+    @PostMapping(path = "/Register")
+    public String RegisterUser(@RequestBody Recruit user){
+        return  recruitUserService.RegisterRecruit(user);
+    }
+}
